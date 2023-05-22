95ab187bdd83b0563540fa9e0ff82b02e392b462
Екатерина <76626037+katerina4cat@users.noreply.github.com>
2023-05-22 04:02:11 +0300

    minimal init

---
+++ src/common/ApiManager.js
@@ -0,0 +1,18 @@
+export class ApiManager{
+
+    constructor(){
+
+
+    }
+
+    async Login(login, password){
+        
+        return false;
+    }
+
+    async CreateAccount(login, password, info){
+
+        return false
+    }
+
+}
